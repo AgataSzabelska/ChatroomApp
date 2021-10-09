810a86cfabe11c28ba8958bf28d45e9bc070c807
ASz <agata.szabelska@gmail.com>
2021-10-09 19:54:05 +0200

    Initial commit

---
+++ src/com/aszabelsk/ClientApp.java
@@ -0,0 +1,8 @@
+package com.aszabelsk;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/aszabelsk/ServerApp.java
@@ -0,0 +1,8 @@
+package com.aszabelsk;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
