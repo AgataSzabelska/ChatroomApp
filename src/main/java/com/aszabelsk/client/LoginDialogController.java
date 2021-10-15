b531394187c1818780d1e1892139751935e66ae5
ASz <agata.szabelska@gmail.com>
2021-10-15 19:20:43 +0200

    Added (still nonfunctional) password field

--- src/main/java/com/aszabelsk/client/LoginDialogController.java
+++ src/main/java/com/aszabelsk/client/LoginDialogController.java
@@ -1,11 +1,15 @@
 package com.aszabelsk.client;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginDialogController {
     @FXML
     private TextField usernameTextField;
+
+    @FXML
+    private PasswordField passwordField;
 
     @FXML
     public void initialize() {
