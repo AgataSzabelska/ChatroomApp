bcb3889ece2226c0e177aed79259ed5e9a93aaaa
ASz <agata.szabelska@gmail.com>
2021-10-15 19:27:51 +0200

    Added tooltips

--- src/main/java/com/aszabelsk/client/ChatWindowController.java
+++ src/main/java/com/aszabelsk/client/ChatWindowController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ChatWindowController {
+
     private final Client client;
 
     private Stage stage;
@@ -23,12 +25,21 @@ public class ChatWindowController {
     private StackPane sendButton;
 
     @FXML
+    private StackPane settingsButton;
+
+    @FXML
+    private StackPane addChatroomButton;
+
+    @FXML
+    private StackPane emojiButton;
+
+    @FXML
     private TextField messageField;
 
     @FXML
     private ListView<String> messageListView; //TODO <Message>
 
-    private final ObservableList<String> messages = FXCollections.emptyObservableList();
+    private final ObservableList<String> messages = FXCollections.observableArrayList();
 
     public ChatWindowController(Stage stage, Client client) {
         this.stage = stage;
@@ -36,8 +47,16 @@ public class ChatWindowController {
         loadFxml();
         initStage();
         initMessageListView();
+        addTooltips();
         registerListeners(client);
         client.start(messages);
+    }
+
+    private void addTooltips() {
+        Tooltip.install(settingsButton, new Tooltip("Settings"));
+        Tooltip.install(addChatroomButton, new Tooltip("Add new chatroom"));
+        Tooltip.install(emojiButton, new Tooltip("Add emoji"));
+        Tooltip.install(sendButton, new Tooltip("Send message"));
     }
 
     private void registerListeners(Client client) {
